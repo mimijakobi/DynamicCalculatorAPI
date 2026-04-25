afb4a1e27de56402a22a93f654a3a69909f89326
Mimi Jakobi <p0033718@ness-tech.co.il>
2026-04-26 02:54:14 +0300

    swagger

--- Program.cs
+++ Program.cs
@@ -32,14 +32,13 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI(c => {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
         c.RoutePrefix = "swagger"; // זה מבטיח שהכתובת תהיה /swagger
     });
-}
+
 
 
 app.UseHttpsRedirection();
